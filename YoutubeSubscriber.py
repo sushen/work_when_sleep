bc749c4f74b074a901a581ae59686ae273f9542a
Sushen Biswas <sushenbiswasaga@gmail.com>
2021-09-11 10:57:06 +0600

    Update: YoutubeSubscriber.py

--- YoutubeSubscriber.py
+++ YoutubeSubscriber.py
@@ -5,6 +5,7 @@ driver = webdriver.Chrome("chromedriver.exe")
 driver.get("https://www.youtube.com/")
 
 # TODO: 2. we need to sign in
+# Learning Carv Xpath https://youtube.com/playlist?list=PLmRg3gEG2XIackdOpGvb_jEX1ywaplUmh
 button_xpath = "//tp-yt-paper-button[@aria-label='Sign in']"
 login_button = driver.find_elements_by_xpath(button_xpath)
 login_button[0].click()
