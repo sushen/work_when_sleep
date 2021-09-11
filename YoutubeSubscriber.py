88c119a3a1cf8f3da47ff0f5b41e924d1db8525e
Sushen Biswas <sushenbiswasaga@gmail.com>
2021-09-11 10:45:20 +0600

    Update: YoutubeSubscriber.py

--- YoutubeSubscriber.py
+++ YoutubeSubscriber.py
@@ -5,5 +5,10 @@ driver = webdriver.Chrome("chromedriver.exe")
 driver.get("https://www.youtube.com/")
 
 # TODO: 2. we need to sign in
+button_xpath = "//tp-yt-paper-button[@aria-label='Sign in']"
+login_button = driver.find_elements_by_xpath(button_xpath)
+login_button[0].click()
+print(len(login_button))
+
 # TODO: 3. we need to subscribe
 # TODO: 4. we need to repeat that things 1000 times
