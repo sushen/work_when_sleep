14aa447a6a1932ea6db0948f72892fdc07845751
Anindoakb <anindo.biswas.232@northsouth.edu>
2026-08-29 05:10:01 +0600

    updated: AllPageBot.py. fixed outdated selenium 4 syntax. updated webdriver initialization. removed deprecated find element methods. makes the bot much more stable.

--- Bots/AllPageBot.py
+++ Bots/AllPageBot.py
@@ -4,13 +4,14 @@ from Bots.bot_GroupMemberPage import Bot_GroupMemberPage
 from Bots.bot_LoginPage import Bot_Login
 from Bots.bot_MessagePage import Bot_MessagePage
 from Bots.bot_ProfilePage import Bot_ProfilePage
-
+from Bots.bot_RedditPage import Bot_RedditPage
 
 class AllPageBot(
     Bot_Login,
     Bot_GroupPage,
     Bot_ProfilePage,
     Bot_MessagePage,
-    Bot_GroupMemberPage
+    Bot_GroupMemberPage,
+    Bot_RedditPage
 ):
     pass
